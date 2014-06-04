c058d42c5cc0829d47dc203613f4ef5c90edab9f
Kotaro Kawashima <utageyanagi+kkgithub@gmail.com>
2014-06-05 00:21:27 +0900

    add sample page as github pages.

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(function(){
+  $("#anim").spriteanimation("init", {
+      width: 480,
+      height: 360,
+      numcol: 5,
+      numrow: 5,
+      maxframe: 23,
+      reverse: false,
+      type: "background"
+  });
+
+  $("#playbtn").on("click", function(){
+    $("#anim").spriteanimation("to", {to: 23, direction: 1, frametime: 2});
+    $("#stopbtn").removeAttr("disabled");
+    $("#playbtn").attr("disabled", "disabled");
+  });
+
+  $("#stopbtn").on("click", function(){
+    $("#anim").spriteanimation("stop");
+    $("#playbtn").removeAttr("disabled");
+    $("#stopbtn").attr("disabled", "disabled");
+  });
+});
---
+++ js/jquery.spriteanimation.js
@@ -0,0 +1,100 @@
+(function(jQuery){
+  jQuery.fn.spriteanimation = function( type, settings){
+
+    var options = jQuery.extend({
+      width: 0,
+      height: 0,
+      numcol: 0,
+      numrow: 0,
+      offset: void 0,
+      maxframe: 0,
+      frame: 0,
+      reverse: false,
+      type: "margin"
+    }, settings);
+
+    return this.each(function(){
+      var t$ = $(this);
+      
+      if(type === "init"){
+        t$.data("spanim", jQuery.extend({}, options));
+      }else if( type === "to"){
+        var data = t$.data("spanim");
+        t$.spriteanimation("stop");
+        var direction = settings.direction || 0;
+        var frametime = settings.frametime || 1;
+        var currentFrameCount = frametime;
+        var normalizeframe = (data.frame - settings.to + data.maxframe) % data.maxframe;
+        if( direction === 0){
+          if( normalizeframe > data.maxframe / 2){
+            direction = 1;
+          }else{
+            direction = -1;
+          }
+        }
+
+        data.offset = (data.offset + data.frame + data.maxframe) % data.maxframe || 0;
+
+        function rotateTo(){
+          currentFrameCount -= 1;
+          if( currentFrameCount === 0){
+            currentFrameCount = frametime;
+            var frame = data.frame + direction;
+            if( data.reverse ){
+              frame = data.maxframe - frame;
+            }
+            frame = (frame + data.maxframe) % data.maxframe;
+            t$.spriteanimation(frame, true);
+            if( rotateTo.playing && frame !== settings.to){
+              requestAnimationFrame(rotateTo);
+            }else{
+              rotateTo.playing = false;
+              t$.trigger("animationStop");
+            }
+          }else{
+            requestAnimationFrame(rotateTo);
+          }
+        }
+
+        rotateTo.playing = true;
+        rotateTo();
+
+        data.currentAnimation = rotateTo;
+
+      }else if( type === "stop"){
+        var data = t$.data("spanim");
+        if( $.isFunction( data.currentAnimation )){
+          data.currentAnimation.playing = false;
+          data.currentAnimation = void 0;
+        }
+      }else if(jQuery.isNumeric( type )){
+        var data = t$.data("spanim");
+        var offset = (settings ? 0 : data.reverse ? data.maxframe - data.offset : data.offset) || 0 ;
+        var frame = ((type << 0) % data.maxframe ) - offset;
+        if( data.reverse ){
+          frame = data.maxframe - frame;
+        }
+        frame = (frame + data.maxframe) % data.maxframe;
+        data.frame = frame;
+        var col = frame % data.numcol;
+        var row = ((frame / data.numcol) << 0 ) % data.numrow;
+
+        var xpos = - (col * data.width);
+        var ypos = - (row * data.height);
+        var pos = xpos + "px " + ypos + "px";
+
+        if( data.type === "margin" ){
+          t$.css({
+            marginLeft: xpos,
+            marginTop: ypos
+          });
+        }else if( data.type === "background" ){
+          var bgp = xpos + "px " + ypos + "px"
+          t$.css({
+            backgroundPosition: bgp
+          });
+        }
+      }
+    });
+  };
+})(jQuery);
